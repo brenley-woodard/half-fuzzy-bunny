87f39985e6391827eb537672422dee845dcff24e
brenley-woodard <brenleyew@gmail.com>
2022-11-30 15:08:24 -0800

    bunnies delete from page (and hopefully supabase also)

--- families/families.js
+++ families/families.js
@@ -10,11 +10,10 @@ logoutButton.addEventListener('click', () => {
 });
 
 async function displayFamilies(families) {
-    // fetch families from supabase
     console.log(families);
-    // clear out the familiesEl
+
     familiesEl.textContent = '';
-    // loop through each family and for each family:
+
     for (let family of families) {
         const familyEl = document.createElement('div');
         const nameEl = document.createElement('h3');
@@ -31,30 +30,19 @@ async function displayFamilies(families) {
             bunnyEl.classList.add('bunny');
             bunnyEl.textContent = bunny.name;
 
+            bunnyEl.addEventListener('click', async () => {
+                await deleteBunny(bunny.id);
+
+                const updatedFamilies = await getFamilies();
+
+                displayFamilies(updatedFamilies);
+            });
             bunniesEl.append(bunnyEl);
         }
 
         familyEl.append(nameEl, bunniesEl);
         familiesEl.append(familyEl);
     }
-
-    // create three elements for each family, one for the whole family, one to hold the name, and one to hold the bunnies
-    // your HTML Element should look like this:
-    // <div class="family">
-    //    <h3>the Garcia family</h3>
-    //    <div class="bunnies">
-    //        <div class="bunny">Fluffy</div>
-    //        <div class="bunny">Bob</div>
-    //    </div>
-    // </div>
-    // add the bunnies css class to the bunnies el, and family css class to the family el
-    // put the family name in the name element
-    // for each of this family's bunnies
-    //    make an element with the css class 'bunny', and put the bunny's name in the text content
-    //    add an event listener to the bunny el. On click, delete the bunny, then refetch and redisplay all families.
-    // append this bunnyEl to the bunniesEl
-    // append the bunniesEl and nameEl to the familyEl
-    // append the familyEl to the familiesEl
 }
 
 window.addEventListener('load', async () => {
